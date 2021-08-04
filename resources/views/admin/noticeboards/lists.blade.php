20a4a09684bba90c0f4de265b5dfa1cf0d206497
Thinh Nguyen <misddev@gmail.com>
2021-08-05 01:00:05 +0700

    no message

--- resources/views/admin/noticeboards/lists.blade.php
+++ resources/views/admin/noticeboards/lists.blade.php
@@ -1,5 +1,9 @@
 @extends('admin.layouts.app')
 
+@push('libraries_top')
+
+@endpush
+
 @section('content')
     <section class="section">
         <div class="section-header">
@@ -7,44 +11,158 @@
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="/admin/">{{trans('admin/main.dashboard')}}</a>
                 </div>
-                <div class="breadcrumb-item">{{ $pageTitle}}</div>
+                <div class="breadcrumb-item">{{ $pageTitle }}</div>
             </div>
         </div>
 
+
         <div class="section-body">
 
-            <div class="row">
-                <div class="col-12 col-md-12">
-                    <div class="card">
-                        <div class="card-body">
+            <section class="card">
+                <div class="card-body">
+                    <form class="mb-0">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label class="input-label">{{trans('admin/main.search')}}</label>
+                                    <input type="text" class="form-control" name="search" value="{{ request()->get('search') }}">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label class="input-label">{{trans('admin/main.start_date')}}</label>
+                                    <div class="input-group">
+                                        <input type="date" id="fsdate" class="text-center form-control" name="from" value="{{ request()->get('from') }}" placeholder="Start Date">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label class="input-label">{{trans('admin/main.end_date')}}</label>
+                                    <div class="input-group">
+                                        <input type="date" id="lsdate" class="text-center form-control" name="to" value="{{ request()->get('to') }}" placeholder="End Date">
+                                    </div>
+                                </div>
+                            </div>
 
 
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label class="input-label">{{trans('admin/main.sender')}}</label>
+                                    <select name="sender" data-plugin-selectTwo class="form-control populate">
+                                        <option value="">Select Sender</option>
+                                        <option value="admin" @if(request()->get('sender') == 'admin') selected @endif>{{trans('admin/main.admin_role')}}</option>
+                                        <option value="organizations" @if(request()->get('sender') == 'organizations') selected @endif>{{trans('admin/main.organizations')}}</option>
+                                    </select>
+                                </div>
+                            </div>
 
-                            <div class="empty-state mx-auto d-block"  data-width="900" >
-                                <img class="img-fluid col-md-6" src="/assets/default/img/plugin.svg" alt="image">
-                                <h3 class="mt-3">This is a paid plugin!</h3>
-                                <h5 class="lead">
-                                You can get this plugin by <strong><a href="mailto:rocketsoftsolutions@gmail.com">contacting support</a></strong> our checking <strong><a href="https://crm.rocket-soft.org">CRM</a></strong>  </h5>                              </p>
-                              </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label class="input-label">{{trans('admin/main.types')}}</label>
+                                    <select name="type" data-plugin-selectTwo class="form-control populate">
+                                        <option value="">{{trans('admin/main.all_types')}}</option>
+
+                                        @foreach(\App\Models\Noticeboard::$adminTypes as $type)
+                                            <option value="{{ $type }}" @if(request()->get('type') == $type) selected @endif>{{ trans('public.'.$type) }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
 
 
-                            
+                            <div class="col-md-4">
+                                <div class="form-group mt-1">
+                                    <label class="input-label mb-4"> </label>
+                                    <input type="submit" class="text-center btn btn-primary w-100" value="{{trans('admin/main.show_results')}}">
+                                </div>
+                            </div>
                         </div>
+                    </form>
+                </div>
+            </section>
 
-                      
+            <div class="card">
+                <div class="card-header">
+                    @can('admin_noticeboards_send')
+                        <div class="text-right">
+                            <a href="/admin/noticeboards/send" class="btn btn-primary">{{trans('admin/main.send_noticeboard')}}</a>
+                        </div>
+                    @endcan
+                </div>
 
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped font-14" id="datatable-basic">
+
+                            <tr>
+                                <th class="text-left">{{ trans('admin/main.title') }}</th>
+                                <th class="text-center">{{ trans('notification.sender') }}</th>
+                                <th class="text-center">{{ trans('site.message') }}</th>
+                                <th class="text-center">{{ trans('admin/main.type') }}</th>
+                                <th class="text-center">{{ trans('admin/main.created_at') }}</th>
+                                <th>{{ trans('admin/main.actions') }}</th>
+                            </tr>
+
+                            @foreach($noticeboards as $noticeboard)
+                                <tr>
+                                    <td class="text-left">{{ $noticeboard->title }}</td>
+                                    <td class="text-center">{{ $noticeboard->sender }}</td>
+
+                                    <td class="text-center">
+                                        <button type="button" data-item-id="{{ $noticeboard->id }}" class="js-show-description btn btn-outline-primary">{{ trans('admin/main.show') }}</button>
+                                        <input type="hidden" value="{{ nl2br($noticeboard->message) }}">
+                                    </td>
+                                    <td class="text-center">{{ trans('admin/main.notification_'.$noticeboard->type) }}</td>
+
+                                    <td class="text-center">{{ dateTimeFormat($noticeboard->created_at,'Y M j | H:i') }}</td>
+
+                                    <td width="100">
+                                        @can('admin_noticeboards_edit')
+                                            <a href="/admin/noticeboards/{{ $noticeboard->id }}/edit" class="btn-transparent  text-primary" data-toggle="tooltip" data-placement="top" title="{{ trans('admin/main.edit') }}">
+                                                <i class="fa fa-edit"></i>
+                                            </a>
+                                        @endcan
+
+                                        @can('admin_notifications_delete')
+                                            @include('admin.includes.delete_button',['url' => '/admin/noticeboards/'. $noticeboard->id.'/delete','btnClass' => ''])
+                                        @endcan
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </table>
                     </div>
+                </div>
+
+                <div class="card-footer text-center">
+                    {{ $noticeboards->links() }}
                 </div>
             </div>
         </div>
     </section>
 
+    <!-- Modal -->
+    <div class="modal fade" id="notificationMessageModal" tabindex="-1" aria-labelledby="notificationMessageLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="notificationMessageLabel">{{ trans('admin/main.contacts_message') }}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
 
-
-
-
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ trans('admin/main.close') }}</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('scripts_bottom')
-
 @endpush
--- resources/views/admin/noticeboards/send.blade.php
+++ resources/views/admin/noticeboards/send.blade.php
@@ -1,5 +1,9 @@
 @extends('admin.layouts.app')
 
+@push('styles_top')
+    <link rel="stylesheet" href="/assets/vendors/summernote/summernote-bs4.min.css">
+@endpush
+
 @section('content')
     <section class="section">
         <div class="section-header">
@@ -7,44 +11,61 @@
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="/admin/">{{trans('admin/main.dashboard')}}</a>
                 </div>
-                <div class="breadcrumb-item">{{ $pageTitle}}</div>
+                <div class="breadcrumb-item">{{ $pageTitle }}</div>
             </div>
         </div>
 
         <div class="section-body">
+            <div class="card">
+                <div class="card-body">
 
-            <div class="row">
-                <div class="col-12 col-md-12">
-                    <div class="card">
-                        <div class="card-body">
+                    <form method="post" action="/admin/noticeboards/{{ !empty($noticeboard) ? $noticeboard->id .'/update' : 'store' }}" class="form-horizontal form-bordered mt-4">
+                        {{ csrf_field() }}
 
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label class="control-label" for="inputDefault">{{ trans('admin/main.title') }}</label>
+                                    <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" value="{{ !empty($noticeboard) ? $noticeboard->title : old('title') }}">
+                                    <div class="invalid-feedback">@error('title') {{ $message }} @enderror</div>
+                                </div>
 
+                                <div class="form-group">
+                                    <label class="control-label">{{ trans('admin/main.type') }}</label>
+                                    <select name="type" id="typeSelect" class="form-control @error('type') is-invalid @enderror">
+                                        <option value="" selected disabled></option>
 
-                            <div class="empty-state mx-auto d-block"  data-width="900" >
-                                <img class="img-fluid col-md-6" src="/assets/default/img/plugin.svg" alt="image">
-                                <h3 class="mt-3">This is a paid plugin!</h3>
-                                <h5 class="lead">
-                                You can get this plugin by <strong><a href="mailto:rocketsoftsolutions@gmail.com">contacting support</a></strong> our checking <strong><a href="https://crm.rocket-soft.org">CRM</a></strong>  </h5>                              </p>
-                              </div>
-
-
-                            
+                                        <option value="all" @if(!empty($noticeboard) and $noticeboard->type == 'all') selected @endif>{{ trans('admin/main.all') }}</option>
+                                        @foreach(\App\Models\Noticeboard::$adminTypes as $type)
+                                            <option value="{{ $type }}" @if(!empty($noticeboard) and $noticeboard->type == $type) selected @endif>{{ trans('admin/main.notification_'.$type) }}</option>
+                                        @endforeach
+                                    </select>
+                                    <div class="invalid-feedback">@error('type') {{ $message }} @enderror</div>
+                                 <div class="text-muted text-small mt-1">{{ trans('admin/main.new_noticeboards_hint') }}</div>
+                                </div>
+                            </div>
                         </div>
 
-                      
+                        <div class="form-group">
+                            <label class="control-label">{{ trans('admin/main.message') }}</label>
+                            <textarea name="message" class="summernote form-control text-left  @error('message') is-invalid @enderror">{{ (!empty($noticeboard)) ? $noticeboard->message :'' }}</textarea>
+                            <div class="invalid-feedback">@error('message') {{ $message }} @enderror</div>
+                        </div>
 
-                    </div>
+
+                        <div class="form-group">
+                            <div>
+                                <button class="btn btn-primary" type="submit">{{ trans('admin/main.send_noticeboard') }}</button>
+                            </div>
+                        </div>
+
+                    </form>
                 </div>
             </div>
         </div>
     </section>
-
-
-
-
-
 @endsection
 
 @push('scripts_bottom')
-
+    <script src="/assets/vendors/summernote/summernote-bs4.min.js"></script>
 @endpush
